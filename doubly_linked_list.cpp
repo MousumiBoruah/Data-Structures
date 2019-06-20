e388182d4b8f70af488cdd4f28b58837f18e3265
MOUSUMI ROY BORUAH <38189142+MousumiBoruah@users.noreply.github.com>
2019-06-20 11:01:31 +0530

    Update doubly_linked_list.cpp
    Insertion operation done.Deletion left

--- doubly_linked_list.cpp
+++ doubly_linked_list.cpp
@@ -29,6 +29,39 @@ void insertion_end(DLL **head,int x){
 	new_node->prev = trav;
 	return;
 }
+
+void insertion_front(DLL **head,int x){
+	DLL *new_node = new DLL();
+	new_node->data = x;
+	//head changed to new node
+	new_node->next = (*head);
+	new_node->prev = NULL;
+
+	if((*head) != NULL){
+		(*head)->prev = new_node;
+	}
+	(*head) = new_node;
+}
+
+void insertion_after(DLL **head,int x,int node_number){
+	DLL *new_node = new DLL();
+	new_node->data = x;
+	DLL *trav = (*head);
+	int count = 1;
+	while( trav->next != NULL){
+		if(count == node_number){
+			new_node->next = trav->next;
+			new_node->next->prev = new_node;
+			trav->next = new_node;
+			new_node->prev = trav;
+			return;
+		}
+		else{
+			trav = trav->next;
+			count++;
+		}
+	}
+}
 print(DLL *head){
 
 	while(head != NULL){
@@ -37,6 +70,26 @@ print(DLL *head){
 	}
 	cout << "NULL" << endl;
 }
+
+
+int main(){
+	DLL *head = NULL;
+
+	insertion_end(&head,1);
+	cout << "INSERTION AT THE END" << endl;
+	print(head);
+	insertion_end(&head,3);
+	cout << "INSERTION AT THE END" << endl;
+	print(head);
+	insertion_front(&head,0);
+	cout << "INSERTION AT THE FRONT" <<endl;
+	print(head);
+	insertion_after(&head,2,2);
+	cout << "INSERTION AFTER NODE 2" <<endl;
+	print(head);
+
+}
+
 
 
 int main(){
